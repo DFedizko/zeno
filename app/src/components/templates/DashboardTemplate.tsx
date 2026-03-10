21ece2d76c779e568e3acfc13f15b002556af77e
DFedizko <fedizkopedro@gmail.com>
2026-03-10 00:23:46 -0300

    feat: creating dashboard template

---
+++ app/src/components/templates/DashboardTemplate.tsx
@@ -0,0 +1,36 @@
+import type { ReactNode } from "react";
+
+interface DashboardTemplateProps {
+  balanceCard: ReactNode;
+  spendingLimit: ReactNode;
+  statsRow: ReactNode;
+  chartsRow: ReactNode;
+  cardSection: ReactNode;
+  cardActions: ReactNode;
+  transactionHistory: ReactNode;
+}
+
+export const DashboardTemplate = ({
+  balanceCard,
+  spendingLimit,
+  statsRow,
+  chartsRow,
+  cardSection,
+  cardActions,
+  transactionHistory,
+}: DashboardTemplateProps) => (
+  <div className="flex gap-5 h-full w-full">
+    <div className="flex flex-col gap-5 flex-1 min-w-0">
+      {balanceCard}
+      {spendingLimit}
+      {statsRow}
+      {chartsRow}
+    </div>
+
+    <div className="flex flex-col gap-5 w-[340px] shrink-0">
+      {cardSection}
+      {cardActions}
+      {transactionHistory}
+    </div>
+  </div>
+);
